48aba86cfe72e3f7c3e6626d675303a3a9b54bd8
Ripupz <rafifadhirajasa88@gmail.com>
2025-12-14 00:35:00 +0700

    add origin for CORS

--- backend/app.py
+++ backend/app.py
@@ -39,6 +39,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://plant-pest-classifier-production.up.railway.app"
 ]
 app.add_middleware(
     CORSMiddleware,
