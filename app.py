9348b2e601367c0105cf6e62ac5f31dd6cd9da3a
Rafif Adhirajasa <rafifadhirajasa88@gmail.com>
2026-01-15 11:21:02 +0700

    fix preprocessing inference

--- app.py
+++ app.py
@@ -56,11 +56,14 @@ def load_model_streamlit(path):
         return None
 
 # --- PREPROCESSING ---
+
+mean = [0.485, 0.456, 0.406]
+std  = [0.229, 0.224, 0.225]
+
 PREPROCESS = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.Resize((224, 224)), # Samakan dengan training: Resize langsung ke 224x224
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    transforms.Normalize(mean, std)
 ])
 
 # --- ANTARMUKA PENGGUNA (UI) ---
